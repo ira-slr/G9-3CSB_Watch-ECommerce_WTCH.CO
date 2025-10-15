9b1ac246669510ec88671966b3095dea2beac507
eneb712 <benedicsarmiento712@gmail.com>
2025-10-15 19:57:57 +0800

    Cart checkout completed.

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./styles/ProductCard.module.css";
 // Member #3 : Josua
-function ProductCard() {
+function ProductCard({ model, brand, star_review, price }) {
   return (
     <div className={styles.card}>
       <div className={styles.imagePlaceholder}></div>
       <div className={styles.info}>
-        <h4 className={styles.productName}>Product Name</h4>
+        <p className={styles.price}>{brand}</p>
+        <h4 className={styles.productName}>{model}</h4>
+        <p className={styles.rating}>Rating: {star_review} ★</p>
         <p className={styles.price}>₱0.00</p>
       </div>
     </div>
